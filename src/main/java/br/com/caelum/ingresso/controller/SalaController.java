d0ac4115b02dfe33dc70643a03631d95d31d0e26
Julivan Meridius <julivan@outlook.com>
2017-06-20 22:24:46 -0300

    implementacao do exercicio 2.4

--- src/main/java/br/com/caelum/ingresso/controller/SalaController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SalaController.java
@@ -1,7 +1,10 @@
 package br.com.caelum.ingresso.controller;
 
 import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -20,6 +25,9 @@ public class SalaController {
 
     @Autowired
     private SalaDao salaDao;
+    
+    @Autowired
+	private SessaoDao sessaoDao;
 
 
     @GetMapping({"/admin/sala", "/admin/sala/{id}"})
@@ -68,6 +76,10 @@ public class SalaController {
         ModelAndView view = new ModelAndView("sessao/lista");
         view.addObject("sala", sala);
 
+
+		List<Sessao> sessoes = sessaoDao.buscaSessoesDaSala(sala);
+		view.addObject("sessoes", sessoes);
+		
         return view;
     }
 
